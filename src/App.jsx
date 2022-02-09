39d4551886554c0e73f7cb46e817e2f10901c4af
emmelleppi <mlp.perego@gmail.com>
2022-02-09 16:37:18 +0100

    changes drei-assets link

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,33 @@ import * as THREE from 'three';
 import SceneManager from './SceneManager';
 import { useStore } from './store';
 import { CustomMaterial } from './materials/CustomMaterial/CustomMaterial';
-import { OrbitControls, useGLTF, useNormalTexture, useTexture } from '@react-three/drei';
+import { OrbitControls, useGLTF, useTexture } from '@react-three/drei';
 import { Leva, useControls } from 'leva';
+
+const NORMAL_ROOT =
+	'https://rawcdn.githack.com/emmelleppi/normal-maps/f24c810fc1d86b5b1e5dfea914b668f70b5f2923';
+const DEFAULT_NORMAL = '151_norm.JPG';
+
+function useNormalTexture(id = 0) {
+	const [normalsList, setNormalsList] = React.useState({});
+	const imageName = normalsList[id] || DEFAULT_NORMAL;
+	const url = `${NORMAL_ROOT}/normals/${imageName}`;
+
+	const normalTexture = useTexture(url);
+
+	React.useEffect(() => {
+		fetch(`${NORMAL_ROOT}/normals.json`)
+			.then((response) => response.json())
+			.then((data) => setNormalsList(data));
+	}, [setNormalsList]);
+
+	React.useEffect(() => {
+		if (!normalTexture) return;
+		normalTexture.wrapS = normalTexture.wrapT = THREE.RepeatWrapping;
+	}, [normalTexture]);
+
+	return [normalTexture];
+}
 
 function Light({ spinning }) {
 	const [light, setLight] = React.useState();
